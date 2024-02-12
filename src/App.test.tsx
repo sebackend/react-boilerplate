3587bcedbf542e105859b5812a68cce9ebcbbc7d
Sebastian Estrada <sbstnmz@gmail.com>
2024-02-12 15:14:01 -0300

    add: NotFound page & unit test example

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,18 +1,29 @@
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
-
-import App from './App';
+import { MemoryRouter } from 'react-router-dom';
+import { App, WrappedApp } from './App';
 
 describe('App', () => {
   it('Renders Hello World', () => {
-    // ARRANGE
-    render(<App />);
-    // ACT
-    // EXPECT
+    render(<WrappedApp />);
     expect(
       screen.getByRole('heading', {
         level: 1,
       })
     ).toHaveTextContent('Hello world');
+  });
+
+  it('Renders not found if invalid path', () => {
+    render(
+      <MemoryRouter initialEntries={['/anything']}>
+        <App />
+      </MemoryRouter>
+    );
+
+    expect(
+      screen.getByRole('heading', {
+        level: 1,
+      })
+    ).toHaveTextContent('Page not found');
   });
 });
